42eb40e35d52a42b6881907c21dde05de1395143
leroy <leroy.fullstack@gmail.com>
2022-11-05 16:55:56 +0900

    add product detail api

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ app.get('/product-list', function (req, res) {
   res.send(JSON.stringify(products));
 });
 
+app.get('/product-detail', function (req, res) {
+  // res.statusCode = 404;
+  // res.end();
+  const product = { name: 'test name' };
+  res.send(JSON.stringify(product));
+});
+
 app.listen(3000);
--- test/get-product-detail.test.js
+++ test/get-product-detail.test.js
@@ -1,8 +1,20 @@
+var request = require('request');
+var expect = require('chai').expect;
+
 var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+describe('Product', function () {
+  describe('Get detail', function () {
+    it('should return item with name property when requested.', function (done) {
+      // assert.equal(['test name'].length, 1);
+      request.get(
+        'http://localhost:3000/product-detail',
+        function (err, res, body) {
+          expect(res.statusCode).to.equal(200);
+          const product = JSON.parse(res.body);
+          assert(product.name, 'test name');
+          done();
+        },
+      );
     });
   });
 });
