5678bfd8dc91411c566ff7520dbde3e8ec0a698b
leroy <leroy.fullstack@gmail.com>
2022-11-05 17:37:14 +0900

    add add-to-cart api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cart = [];
 
 app.get('/', function (req, res) {
   res.send('Hello World');
@@ -15,6 +16,16 @@ app.get('/product-detail', function (req, res) {
   // res.end();
   const product = { name: 'test name' };
   res.send(JSON.stringify(product));
+});
+
+app.get('/add-to-cart/:productName', function (req, res) {
+  // res.statusCode = 404;
+  // res.end();
+  console.log(JSON.stringify(req.params, null, 2));
+  const productName = req.params['productName'];
+  cart.push(productName);
+
+  res.send(productName);
 });
 
 app.listen(3000);
--- test/add-to-cart.test.js
+++ test/add-to-cart.test.js
@@ -1,8 +1,19 @@
+var request = require('request');
+var expect = require('chai').expect;
 var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+describe('Cart', function () {
+  describe('add product', function () {
+    it('should return product name when request is suceeded.', function (done) {
+      request.get(
+        'http://localhost:3000/add-to-cart/testName',
+        function (err, res, body) {
+          expect(res.statusCode).to.equal(200);
+          const productName = res.body;
+          assert(productName, 'testName');
+          done();
+        },
+      );
     });
   });
 });
